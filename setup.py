c455935cc6727424faef059f1961cfd437a2ffef
jxzper <jesper.hoogenberg@gmail.com>
2022-11-04 20:47:28 +0100

    version bump to 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url="https://github.com/jxzper/cordstore",
     license='MIT',
     description='Utilize discord to store files in textchannels using bots or webhooks',
-    version="0.0.1",
+    version="0.0.2",
     install_requires=[
         "discord",
         "aiohttp",
